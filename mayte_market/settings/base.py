b2068ffaa5f8806fdac3a6461def0d628e26bd3b
Percy Bellido <pbellido0401@gmail.com>
2026-04-28 17:31:54 -0500

    Config: usar variables de entorno con decouple

--- mayte_market/settings/base.py
+++ mayte_market/settings/base.py
@@ -1,3 +1,5 @@
+from decouple import config
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,10 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-59eqls(lz3kra=4bciiuh__owk18e_nbqppm_054v$%aw&+2c1'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # Application definition
 
